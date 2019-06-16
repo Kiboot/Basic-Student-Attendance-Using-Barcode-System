916ba335930f6721e5a10f20bed3b6f1b62eb212
Kiboot <kiboot@users.noreply.github.com>
2019-06-16 21:50:13 +0800

    Functionality Test

--- app/src/main/java/kibs/mcm/edu/ph/bsaub/MainActivity.java
+++ app/src/main/java/kibs/mcm/edu/ph/bsaub/MainActivity.java
@@ -1,13 +1,33 @@
 package kibs.mcm.edu.ph.bsaub;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn1);
+        button.setOnClickListener(this);
+
     }
+    @Override
+    public void onClick(View arg0) {
+
+        // Start NewActivity.class
+        Intent myIntent = new Intent(this, QrCodeScannerActivity.class);
+        startActivity(myIntent);
+
+
+    }
+
 }
