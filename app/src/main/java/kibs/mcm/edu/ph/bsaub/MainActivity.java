3f86c27c7c9767e672870990c4faea0153b78cb1
Kiboot <kiboot@users.noreply.github.com>
2019-06-16 19:51:47 +0800

    Project refactored to AndroidX

--- app/src/main/java/kibs/mcm/edu/ph/bsaub/MainActivity.java
+++ app/src/main/java/kibs/mcm/edu/ph/bsaub/MainActivity.java
@@ -1,6 +1,6 @@
 package kibs.mcm.edu.ph.bsaub;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
