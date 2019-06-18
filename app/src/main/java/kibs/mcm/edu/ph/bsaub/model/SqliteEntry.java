e64f1a163e1f6d042e725912414afe3cc974f59e
Kiboot <kiboot@users.noreply.github.com>
2019-06-18 19:36:19 +0800

    Further code refactoring
    cleaning code for readability. Preparing the program for SQLite integration

--- app/src/main/java/kibs/mcm/edu/ph/bsaub/model/SqliteEntry.java
+++ app/src/main/java/kibs/mcm/edu/ph/bsaub/model/SqliteEntry.java
@@ -4,10 +4,12 @@ public class SqliteEntry {
     public static final String TABLE_NAME = "studtbl";
 
     public static final String COLUMN_ID = "id";
+    public static final String COLUMN_SID = "sid";
     public static final String COLUMN_NAME = "name";
     public static final String COLUMN_TIMESTAMP = "timestamp";
 
     private int id;
+    private int sid;
     private String studname;
     private String timestamp;
 
@@ -15,7 +17,8 @@ public class SqliteEntry {
     // Create table SQL query
     public static final String CREATE_TABLE =
             "CREATE TABLE " + TABLE_NAME + "("
-                    + COLUMN_ID + " INTEGER PRIMARY KEY,"
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_SID + " INTEGER,"
                     + COLUMN_NAME + " TEXT,"
                     + COLUMN_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP"
                     + ")";
@@ -23,8 +26,9 @@ public class SqliteEntry {
     public SqliteEntry() {
     }
 
-    public SqliteEntry(int id, String name, String timestamp) {
+    public SqliteEntry(int id, int sid, String name, String timestamp) {
         this.id = id;
+        this.sid = sid;
         this.studname = name;
         this.timestamp = timestamp;
     }
@@ -33,11 +37,15 @@ public class SqliteEntry {
         return id;
     }
 
-    public String getNote() {
+    public int getSId() {
+        return sid;
+    }
+
+    public String getSname() {
         return studname;
     }
 
-    public void setNote(String name) {
+    public void setName(String name) {
         this.studname = name;
     }
 
@@ -47,6 +55,10 @@ public class SqliteEntry {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public void setSId(int sid) {
+        this.sid = sid;
     }
 
     public void setTimestamp(String timestamp) {
